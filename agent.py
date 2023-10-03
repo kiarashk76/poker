1558dab93a54d46165ed75d7babac3ee3c8cb433
kiarash aghakasiri <kiarash@kiarashs-NewMacBook-Pro.local>
2023-10-02 21:21:19 -0600

    add name to agents

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ class RandomAgent(object):
     ''' A random agent. Random agents is for running toy examples on the card games
     '''
 
-    def __init__(self, num_actions):
+    def __init__(self, num_actions, name):
         ''' Initilize the random agent
 
         Args:
@@ -15,7 +15,8 @@ class RandomAgent(object):
         '''
         self.use_raw = False
         self.num_actions = num_actions
-
+        self.name = name
+        
     @staticmethod
     def step(state):
         ''' Predict the action given the curent state in gerenerating training data.
@@ -28,23 +29,4 @@ class RandomAgent(object):
         '''
         return np.random.choice(list(state['legal_actions'].keys()))
 
-    def eval_step(self, state):
-        ''' Predict the action given the current state for evaluation.
-            Since the random agents are not trained. This function is equivalent to step function
-
-        Args:
-            state (dict): An dictionary that represents the current state
-
-        Returns:
-            action (int): The action predicted (randomly chosen) by the random agent
-            probs (list): The list of action probabilities
-        '''
-        probs = [0 for _ in range(self.num_actions)]
-        for i in state['legal_actions']:
-            probs[i] = 1/len(state['legal_actions'])
-
-        info = {}
-        info['probs'] = {state['raw_legal_actions'][i]: probs[list(state['legal_actions'].keys())[i]] for i in range(len(state['legal_actions']))}
-
-        return self.step(state), info
 
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import rlcard
 from agent import RandomAgent
 from env import ModifiedEnvironment
 
-env = ModifiedEnvironment(3, [6, 6, 6])
-agents = [RandomAgent(num_actions=env.num_actions), 
-            RandomAgent(num_actions=env.num_actions),
-            RandomAgent(num_actions=env.num_actions)]
+env = ModifiedEnvironment(3, [3, 3, 3])
+agents = [RandomAgent(num_actions=env.num_actions, name="mohsen"), 
+            RandomAgent(num_actions=env.num_actions, name="kiarash"),
+            RandomAgent(num_actions=env.num_actions, name="hamza")]
 env.set_agents(agents)
 env.reset()
 
@@ -14,6 +14,11 @@ while not env.is_over():
     curr_state = env.get_state(curr_player_id)
     curr_player = agents[curr_player_id]
     action = curr_player.step(curr_state)
+    print(f"Money: {env.all_money}")
+    print(f"player {curr_player.name} action {action}")
     # print(f"player: {curr_player_id}, action: {action}, state: {curr_state['raw_obs']}")
     env.step(action)
+    input("")
+print(f"Money:{env.all_money}")
+
     
